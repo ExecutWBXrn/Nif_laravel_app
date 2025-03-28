fd2a0fe825783b7d01f43b70c8aa20e4abd3ab93
unknown <executwbxrn@gmail.com>
2025-03-28 15:41:52 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    <div class="flex w-full h-full border">
+        <div class="my-auto">
+            <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/img/welcome/ivano-frankivsk-oblast-map.png') }}" alt="">
+        </div>
+    </div>
+</x-layout>
